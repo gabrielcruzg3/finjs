c7fa9ed56ff505e5feba9f7fe406da080033fac0
gabrielcruzg3 <cruz.gabrielg3@gmail.com>
2021-11-27 00:02:39 -0300

    procura funcional

---
+++ script/script.js
@@ -0,0 +1,157 @@
+function dbTesteBkp(){
+    delDB();
+    const cursosJsonDB = 
+    [
+        {
+            id: '0',
+            titulo: 'RUMO AO BILHÃO #8 | COMPREI BITCOIN! (BCFF12 e SMALL CAP também!)',
+            descricao: 'ABRA SUA CONTA GRATUITAMENTE NA XDEX: http://bit.ly/2vYMOljOUÇA O PRIMOCAST: htstraseprim...',
+            imagem: 'https://i.ytimg.com/vi/rHzzveyJTco/maxresdefault.jpg',
+            professor: 'Thiago Nigro',
+            listaDeAulas: 'https://www.youtube.com/embed/rHzzveyJTco',
+            index: '0'
+
+        }, 
+        {
+            id: '2',
+            titulo: 'RUMO AO BILHÃO #9',
+            descricao: 'ABRA SUA CONTA Rico: https://bit.ly/cadastraseprim...',
+            imagem: 'https://i.ytimg.com/vi/rHzzveyJTco/maxresdefault.jpg',
+            professor: 'Thiago Nigro',
+            listaDeAulas: 'https://www.youtube.com/embed/aA-BlLzPYi0',
+            index: '1'
+        }, 
+        {
+            id: '3',
+            titulo: 'RUMO AO BILHÃO #9',
+            descricao: 'ABRA SUA CONTA Rico: https://bit.ly/cadastraseprim...',
+            imagem: 'https://i.ytimg.com/vi/rHzzveyJTco/maxresdefault.jpg',
+            professor: 'Thiago Nigro',
+            listaDeAulas: 'https://www.youtube.com/embed/aA-BlLzPYi0',
+            index: '1'
+        }, 
+        {
+            id: '4',
+            titulo: 'RUMO AO BILHÃO #9',
+            descricao: 'ABRA SUA CONTA Rico: https://bit.ly/cadastraseprim...',
+            imagem: 'https://i.ytimg.com/vi/rHzzveyJTco/maxresdefault.jpg',
+            professor: 'Thiago Nigro',
+            listaDeAulas: 'https://www.youtube.com/embed/aA-BlLzPYi0',
+            index: '1'
+        }
+    ]
+
+    localStorage.setItem('cursosDB', JSON.stringify(cursosJsonDB))
+
+}
+///////////////////////////////////////////////////
+
+
+const getDB = () => JSON.parse(localStorage.getItem('cursosDB')) ?? [];
+const setDB = (cursosJsonDB) => localStorage.setItem('cursosDB', JSON.stringify(cursosJsonDB));
+const delDB = () => {
+    localStorage.removeItem('cursosDB'); 
+    limpaTable();
+}
+
+const formPreenchido = () => {
+        return document.getElementById('form').reportValidity();
+    }
+
+const cursoForm = () => {
+    return {
+        id: document.getElementById('id').value,
+        titulo: document.getElementById('title').value,
+        descricao: document.getElementById('description').value,
+        imagem: document.getElementById('image').value,
+        professor: document.getElementById('teacher').value,
+        listaDeAulas: document.getElementById('linkList').value
+    };
+}
+
+
+const limpaTable = () => document.querySelectorAll('#tableCursos>tbody>tr').forEach(tr => tr.parentNode.removeChild(tr));
+
+function criarCurso(){
+
+    if (formPreenchido() == false){
+        alert("Por favor preencha os campos!")
+
+    } 
+    if (formPreenchido() == true) {
+        alert("tá tudo ok")
+
+        const cursosJsonDB = getDB();
+        cursosJsonDB.push(cursoForm());
+        setDB(cursosJsonDB);
+    }
+
+    // readCursos();
+}
+
+function exibirCursos(){
+
+    limpaTable()
+
+    const idSearch = document.getElementById('cursoSearch').value;
+let index = 0;
+    do{
+        if(idSearch == getDB()[index].id){
+
+            const curso = getDB()[index]
+            criaTr(curso);     
+            break
+        }
+        if (index == getDB().length - 1) {
+
+            alert('not find given id in the database')
+            break 
+        }
+       
+         index++; 
+    }while(index <= getDB().length)
+
+}
+
+function listarCursos(){  //esse ´o PROBLEMAAAAAAAAAAAAAAAAAAAAAAAA
+
+    if (getDB() == []) {
+       limpaTable();        
+    }
+    else
+    {
+        limpaTable();
+
+        
+
+        getDB().forEach(curso => {
+
+            criaTr(curso);
+
+        })  
+    }
+    
+
+    
+}
+
+function criaTr(curso) {
+        const trLista = document.createElement('tr');
+        trLista.innerHTML = `     
+            <td> ${curso.id}  </td>
+            <td> ${curso.index} </td>
+            <td> ${curso.titulo} </td>
+            <td> ${curso.descricao} </td>
+            <td> <img width="160" height="80" src="${curso.imagem}"> </td>
+            <td> ${curso.professor} </td>
+            <td><iframe width="480" height="270" src="${curso.listaDeAulas.replace("watch?v=", "embed/")}" title="YouTube video player" frameborder="0" allowfullscreen></iframe></td>
+            <td> <input type="button" id="editButton${curso.id}" value="editar curso" onclick="updateCursos()"> </td>
+            <td> <input type="button" id="deletetButton${curso.id}" value="excluir curso" onclick="deleteCursos()"> </td>
+        `;
+        document.querySelector('#tableCursos>tbody').appendChild(trLista);
+    }
+
+
+function deleteCursos(){
+
+}
